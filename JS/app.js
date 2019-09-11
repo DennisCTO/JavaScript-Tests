0247b89c20be71e05e90e9ebc18b185e6857100d
Dennis <denniskravets@gmail.com>
2019-09-10 22:10:59 -0700

    First JS examples built for Button functionality

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+function myFirstFunction() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("green_text").innerHTML = result;
+}
